56a2c6d9f029bd16fa89b03bf6f4c389769aea13
Drew Blake <drew.blake@lonelyplanet.com>
2020-05-16 20:38:57 -0500

    Initial Commit

---
+++ Sources/LogStore/LogViewController.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Drew Blake on 5/16/20.
+//
+
+import UIKit
+
+public class LogViewController : UITableViewController {
+    
+    let logItems = LogStore.log
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return logItems.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = logItems[indexPath.row]
+        
+        return cell
+    }
+}
